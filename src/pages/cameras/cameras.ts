d39d4f3181e2333e7f6cdfcba2c48173b2067c7f
Sam Munro <emberDesignLtd@protonmail.com>
2021-05-05 14:59:25 +0100

    feat(Cameras): Add camera page/class which responds to mouse coords

--- src/pages/cameras/cameras.ts
+++ src/pages/cameras/cameras.ts
@@ -1,3 +1,6 @@
+import * as THREE from 'three';
+import { createTestCube } from '../../functions/three_js_helpers';
+import { MouseCoordinates } from './../../classes/mouse_cordinates';
 import { VanillaCanvas } from './../../classes/vanilla_canvas';
 
 enum Element {
@@ -6,7 +9,27 @@ enum Element {
 
 export class ThreeCameraPlayground {
   canvas: VanillaCanvas;
+  testCube: THREE.Mesh;
+  mouseCoordinates = new MouseCoordinates();
+  cursorPosition = { x: 0, y: 0 };
+
   constructor() {
+    this.mouseCoordinates.coordinates$.subscribe((cursorPosition) => {
+      this.cursorPosition = cursorPosition;
+    });
     this.canvas = new VanillaCanvas(Element.CAMERA_CANVAS);
+    this.testCube = createTestCube(this.canvas.scene);
+    this.tick();
+  }
+
+  private tick(): void {
+    this.canvas.perspectiveCamera.position.x = Math.sin(this.cursorPosition.x * Math.PI * 2) * 10;
+    this.canvas.perspectiveCamera.position.z = Math.cos(this.cursorPosition.x * Math.PI * 2) * 10;
+    this.canvas.perspectiveCamera.position.y = this.cursorPosition.y * 20;
+    this.canvas.perspectiveCamera.lookAt(this.testCube.position);
+    if (this.canvas.renderer) {
+      this.canvas.renderer.render(this.canvas.scene, this.canvas.perspectiveCamera);
+    }
+    requestAnimationFrame(this.tick.bind(this));
   }
 }
