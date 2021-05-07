f64cef91873c80067dd3cde1bc7a55a2032f4662
Sam Munro <emberDesignLtd@protonmail.com>
2021-05-07 16:05:43 +0100

    feat(VanillaCanvas): Add basic lighting function

--- src/classes/vanilla_canvas.ts
+++ src/classes/vanilla_canvas.ts
@@ -46,4 +46,14 @@ export class VanillaCanvas {
 
     resizeCanvasListener(this.renderer, this.perspectiveCamera);
   }
+
+  addSceneLighting(): void {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    const pointLight = new THREE.PointLight(0xffffff, 0.5);
+    pointLight.position.x = 2;
+    pointLight.position.y = 3;
+    pointLight.position.z = 4;
+    this.scene.add(ambientLight);
+    this.scene.add(pointLight);
+  }
 }
