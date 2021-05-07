7de7ba8e8bfebe8da57891bcf38ba213f74da705
Sam Munro <emberDesignLtd@protonmail.com>
2021-05-07 13:09:22 +0100

    feat(Material&Textures): Add plane,sphere & torus to scene

--- src/pages/material_and_textures/material_and_textures.ts
+++ src/pages/material_and_textures/material_and_textures.ts
@@ -1,4 +1,7 @@
+import * as THREE from 'three';
+import { DatGuiHelper } from '../../classes/dat_gui_helper';
 import { VanillaCanvas } from './../../classes/vanilla_canvas';
+import { createPlane, createSphere, createTorus } from './../../functions/three_js_helpers';
 
 enum Element {
   TEXTURE_CANVAS = 'material_and_textures',
@@ -9,10 +12,34 @@ export class MaterialAndTextureExample {
     Element.TEXTURE_CANVAS
   ) as HTMLCanvasElement;
   private readonly canvas = new VanillaCanvas(this.canvasElement, true);
+  private readonly sphere = createSphere(this.canvas.scene);
+  private readonly plane = createPlane(this.canvas.scene);
+  private readonly torus = createTorus(this.canvas.scene);
+  private readonly group = new THREE.Group();
+  private readonly datGui = new DatGuiHelper();
 
   constructor() {
     if (!this.canvasElement) return;
+    this.datGui.addAllControls(this.plane);
+    this.spaceMeshesOut();
+    this.addMeshesToGroup();
+    this.canvas.scene.add(this.group);
     this.tick();
+  }
+
+  private addMeshesToGroup() {
+    this.group.add(this.plane);
+    this.group.add(this.torus);
+    this.group.add(this.sphere);
+    this.group.position.x = -2;
+  }
+
+  private spaceMeshesOut() {
+    this.plane.position.x = 2;
+    this.torus.position.x = 4;
+    this.sphere.position.y = 4.5;
+    this.plane.position.y = 4.5;
+    this.torus.position.y = 4.5;
   }
 
   private tick(): void {
