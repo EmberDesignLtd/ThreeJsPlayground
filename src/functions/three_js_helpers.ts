1f8a4819078567f8db6830b6bbba69bd5ff78a60
Sam Munro <emberDesignLtd@protonmail.com>
2021-05-07 13:08:51 +0100

    feat(ThreeHelpers): Add torus, sphere, & plane creator functions

--- src/functions/three_js_helpers.ts
+++ src/functions/three_js_helpers.ts
@@ -40,10 +40,38 @@ export const createCube = (
   scene: THREE.Scene,
   material = new THREE.MeshBasicMaterial({ color: Colour.RED })
 ): THREE.Mesh => {
-  const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
-  const mesh = new THREE.Mesh(boxGeometry, material);
-  mesh.rotation.x = 0.2;
-  mesh.rotation.y = 0.5;
+  const mesh = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), material);
+  mesh.position.y = 4;
   scene.add(mesh);
   return mesh;
+};
+
+export const createSphere = (
+  scene: THREE.Scene,
+  material = new THREE.MeshBasicMaterial({ color: Colour.RED })
+): THREE.Mesh => {
+  const sphereMesh = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+  sphereMesh.position.y = 4;
+  scene.add(sphereMesh);
+  return sphereMesh;
+};
+
+export const createPlane = (
+  scene: THREE.Scene,
+  material = new THREE.MeshBasicMaterial({ color: Colour.RED })
+): THREE.Mesh => {
+  const planeMesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+  planeMesh.position.y = 4;
+  scene.add(planeMesh);
+  return planeMesh;
+};
+
+export const createTorus = (
+  scene: THREE.Scene,
+  material = new THREE.MeshBasicMaterial({ color: Colour.RED })
+): THREE.Mesh => {
+  const torusMesh = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 16, 32), material);
+  torusMesh.position.y = 4;
+  scene.add(torusMesh);
+  return torusMesh;
 };
