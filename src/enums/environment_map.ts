8772c57e4c65c056e7ad2c59a190051336cfcbde
Sam Munro <emberDesignLtd@protonmail.com>
2021-05-07 17:14:57 +0100

    feat(Enums): Add environment maps enum

---
+++ src/enums/environment_map.ts
@@ -0,0 +1,17 @@
+export enum EnvironmentMap0 {
+  NX = '../../assets/textures/environment_maps/0/nx.jpg',
+  NY = '../../assets/textures/environment_maps/0/ny.jpg',
+  NZ = '../../assets/textures/environment_maps/0/nz.jpg',
+  PX = '../../assets/textures/environment_maps/0/px.jpg',
+  PY = '../../assets/textures/environment_maps/0/py.jpg',
+  PZ = '../../assets/textures/environment_maps/0/pz.jpg',
+}
+
+export enum EnvironmentMap1 {
+  NX = '../../assets/textures/environment_maps/1/nx.jpg',
+  NY = '../../assets/textures/environment_maps/1/ny.jpg',
+  NZ = '../../assets/textures/environment_maps/1/nz.jpg',
+  PX = '../../assets/textures/environment_maps/1/px.jpg',
+  PY = '../../assets/textures/environment_maps/1/py.jpg',
+  PZ = '../../assets/textures/environment_maps/1/pz.jpg',
+}
