2754b1c055444baed59de38c0227c2a364bb35bc
Sam Munro <emberDesignLtd@protonmail.com>
2021-05-11 17:16:44 +0100

    feat(Enums): Add particle texture path

---
+++ src/enums/particles_textures.ts
@@ -0,0 +1,16 @@
+// TODO(Munro): Update to more reasonable names and the files themselves
+export enum ParticleTexture {
+  A = '../../assets/particle_textures/1.png',
+  B = '../../assets/particle_textures/2.png',
+  C = '../../assets/particle_textures/3.png',
+  D = '../../assets/particle_textures/4.png',
+  E = '../../assets/particle_textures/5.png',
+  F = '../../assets/particle_textures/6.png',
+  G = '../../assets/particle_textures/7.png',
+  H = '../../assets/particle_textures/8.png',
+  I = '../../assets/particle_textures/9.png',
+  J = '../../assets/particle_textures/10.png',
+  K = '../../assets/particle_textures/11.png',
+  L = '../../assets/particle_textures/12.png',
+  M = '../../assets/particle_textures/13.png',
+}
